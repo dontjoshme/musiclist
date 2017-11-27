ad8356890229cfa8a8e7514a3ace1eb1aed687f4
Joshua Mullenberg <dontjoshme@me.com>
2017-11-27 05:00:32 -0600

    Built Login Page

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/musiclist.scss';
 import { AppContainer } from 'react-hot-loader';
 import Template from './components/Template';
 
